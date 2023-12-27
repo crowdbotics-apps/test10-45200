e988b9ac535a68d5bcc311b014338f0dad0f3fed
Raj sharma <kevinrinta2018@gmail.com>
2023-12-27 20:01:20 +0530

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- screens/welcome/index.js
+++ screens/welcome/index.js
@@ -1,64 +1,45 @@
-import React from "react"
-import {
-  View,
-  Image,
-  Text,
-  ScrollView,
-  SafeAreaView,
-  StyleSheet
-} from "react-native"
+import React, { useState } from "react";
+import { SafeAreaView, StyleSheet, TextInput, Button, Alert } from "react-native";
+import axios from "axios";
+import { useNavigation } from "@react-navigation/native";
 
-const WelcomeScreen = () => {
-  return (
-    <SafeAreaView style={styles.container}>
-      <ScrollView contentContainerStyle={styles.scrollView}>
-        <View style={styles.group} />
-        <View style={styles.group}>
-          <Image style={styles.logo} source={require("./logo.png")} />
-          <Text style={styles.text}>
-            Let's build something amazing together!
-          </Text>
-        </View>
-        <Text style={styles.footer}>Made with ❤️ by Crowdbotics</Text>
-      </ScrollView>
-    </SafeAreaView>
-  )
-}
+const LoginScreen = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const navigation = useNavigation();
+
+  const handleLogin = () => {
+    axios.post("https://dummyjson.com/auth/login", {
+      username: username,
+      password: password
+    }).then(response => {
+      if (response.status === 200) {
+        navigation.navigate("NewScreen");
+      }
+    }).catch(error => {
+      Alert.alert("Login failed", error.message);
+    });
+  };
+
+  return <SafeAreaView style={styles.container}>
+      <TextInput style={styles.input} placeholder="Username" value={username} onChangeText={setUsername} />
+      <TextInput style={styles.input} placeholder="Password" value={password} onChangeText={setPassword} secureTextEntry />
+      <Button title="Login" onPress={handleLogin} />
+    </SafeAreaView>;
+};
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: "#F8F8FC",
-    height: "100%"
-  },
-  scrollView: {
     flex: 1,
-    alignItems: "center",
-    justifyContent: "space-between",
-    padding: 20
+    justifyContent: "center",
+    padding: 16
   },
-  group: {
-    alignItems: "center"
-  },
-  logo: {
-    height: 180,
-    width: 180,
-    padding: 40,
-    borderRadius: 30,
-    margin: 40
-  },
-  text: {
-    textAlign: "center",
-    fontSize: 28,
-    color: "#828AB0",
-    fontWeight: 700
-  },
-  footer: {
-    textAlign: "center",
-    fontSize: 18,
-    color: "#828AB0",
-    fontWeight: 700,
-    marginBottom: 20
+  input: {
+    height: 40,
+    borderColor: "gray",
+    borderWidth: 1,
+    marginBottom: 10,
+    padding: 8
   }
-})
-
-export default WelcomeScreen
+});
+export default LoginScreen;
